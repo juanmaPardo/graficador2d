f823dce3e630242f77ca820467e427d176ded423
juanmaPardo <pardojuanmanuel99@gmail.com>
2020-08-11 14:57:39 -0300

    Inicializacion hecha

--- graficador.py
+++ graficador.py
@@ -3,3 +3,12 @@ import numpy as np
 
 from matplotlib import pyplot as ppl
 import seaborn as sb
+
+class Graficador:
+    def __init__(self,filas,col):
+        self.figura, self.axes = ppl.subplots(filas,col,figsize=(8.6,6))
+        self.filas = filas
+        self.col = col
+
+    
+

8866a85441a0555f084882b6cbc8ef294ba2113e
juanmaPardo <pardojuanmanuel99@gmail.com>
2020-09-03 19:48:28 -0300

    Se agrego la funcionalidad que grafica dos histogramas uno arriba del otro con objetivos comparativos

--- graficador.py
+++ graficador.py
@@ -79,10 +79,53 @@ class Graficador:
     def histograma(self,x,bins=None,tam_barra=None,color_barra="#5d82c9",color_borde="black",
                    orientacion="vertical",label=None,posLegend="upper right",titulo=None,
                    display_bins_ranges=False):
+        """
+        Realiza el grafico de un histograma en la axis correspondiente con la informacion que se
+        le otorgo por parametro.
+
+        :param x: Vector de valores observados
+        :param bins: Una lista con bins custom
+        :param tam_barra: Tamaño relativo de la barra
+        :param color_barra: Color de la barra
+        :param color_borde: El color del borde de las barras del histograma
+        :param orientacion: vertical por default, horizontal si lo queres al revez
+        :param label: Un string que represente el label del grafico
+        :param posLegend: upper/lower/center + left/right todas las combinaciones
+        :param title: Titulo del grafico
+        :param display_bins_ranges: True si queres que se muestren todos los rangos que determinan cada bien
+                                    ya sea en el eje X o en el eje Y
+        """
         ax = self.obt_ax()
         ax.set_title(titulo)
         (_,bins,_) = ax.hist(x,bins=bins,rwidth=tam_barra,orientation=orientacion,fc=color_barra,ec=color_borde)
         indices = bins[[i%2 == 0 for i in range(len(bins))]] if not display_bins_ranges else bins
+        ppl.sca(ax)
+        ppl.xticks(indices) if orientacion=="vertical" else ppl.yticks(indices)
+        if label:
+            ax.legend(loc=posLegend)
+
+    def histograma_conjunto(self,x1,x2,bins1=None,bins2=None,cbarrras=["#fcb27c","#5d82c9"],cbordes=["black","black"],display_bins_ranges=False,
+                  orientacion="vertical",label=[None,None],posLegend="upper right",titulo=None):
+        """
+        Grafica dos histogramas uno arriba del otro con objetivos de comparar resultados
+        :param x1: Set de datos 1
+        :param x2: Set de datos 2
+        :param bins1: Bins custom para set de datos numero 1
+        :param bins2: Bins custom para set de datos numero 2
+        :param cbarrras: Lista que se usara para darle el color a las barras de los histogramas
+        :param cbordes:  Lista que se usara para darle el color al contorno de las barras de los histogramas
+        :param display_bins_ranges: True en caso que quieras ver todos los rangos de los dos histogramas
+        :param orientacion: Vertical/Horizontal
+        :param label: Lista de dos elementos que se usara para agregarle los labels a los histogramas
+        :param posLegend: Posicion en donde queres que se encuentre la leyenda
+        :param titulo: Titulo del grafico
+        """
+        ax = self.obt_ax()
+        ax.set_title(titulo)
+        (_,bins1,_) = ax.hist(x1,bins=bins1,alpha=0.8,rwidth=0.97,orientation=orientacion,fc=cbarrras[0],ec=cbordes[0],label=label[0])
+        (_,bins2,_) = ax.hist(x2,bins=bins2,alpha=0.5,orientation=orientacion,fc=cbarrras[1],ec=cbordes[1],label=label[1])
+        conc_bins = np.sort(np.concatenate((bins1,bins2),axis=None))
+        indices = conc_bins[[i%2 == 0 for i in range(len(conc_bins))]] if not display_bins_ranges else conc_bins
         ppl.sca(ax)
         ppl.xticks(indices) if orientacion=="vertical" else ppl.yticks(indices)
         if label:
@@ -101,6 +144,7 @@ linea.set_color_punto("#f45ddc")
 linea.set_tamanio_punto(20)
 linea.set_color_contorno_punto("#ffffff")
 
+# Set de graficos Numero uno #
 #Instancio clase
 graficador = Graficador(1,3,estilo=["seaborn"])
 
@@ -110,9 +154,12 @@ graficador.g2d_graficar([1,2,3,4,5,6],[1,2,3,4,5,6],linea,"Dotted line","upper l
 #Realizo Grafico Dos
 graficador.histograma([1,1,2,3,2,5,6,2,1.5,6,4],titulo="Prueba histograma")
 
+#Realizo Grafico Tres
+graficador.histograma_conjunto([1,1,2,3,2,5,6,2,1.5,6,6],[1,3,3,2,1,3,6,8,4,5,6,4,1,2],label=["H1","H2"],titulo="Histogramas conjuntos")
+
 #Hago un display de los graficos
 graficador.display_graficos()
 
-
+# Set de graficos Numero dos #
     
 
